7bccfb3111018afcf84ed2e5a86d2b95cf408250
floor12 <primelifecoder@gmail.com>
2018-02-10 12:06:12 +0100

    skip first file in fileblock

--- src/components/FilesBlock.php
+++ src/components/FilesBlock.php
@@ -19,11 +19,15 @@ class FilesBlock extends InputWidget
     public $title;
     public $zipTitle = 'files';
     public $downloadAll = false;
+    public $passFirst = false;
 
     public function run()
     {
         FilesBlockAsset::register($this->getView());
         $this->getView()->registerJs("yiiDownloadAllLink = '" . Url::toRoute('files/default/zip') . "'", \yii\web\View::POS_BEGIN, 'yiiDownloadAllLink');
+
+        if ($this->passFirst && sizeof($this->files) > 0)
+            $this->files = array_slice($this->files, 1);
 
         if ($this->files)
             return $this->render('filesBlock', [
