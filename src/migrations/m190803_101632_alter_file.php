6f78c30d45220670b270de3dc0deb977ce7f95a0
floor12 <floor12@floor12.net>
2019-08-03 13:03:30 +0200

    Indexes added to file table.

---
+++ src/migrations/m190803_101632_alter_file.php
@@ -0,0 +1,33 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m190803_101632_alter_file
+ */
+class m190803_101632_alter_file extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createIndex('file-class', 'file', 'class');
+        $this->createIndex('file-object_id', 'file', 'object_id');
+        $this->createIndex('file-field', 'file', 'field');
+        $this->createIndex('file-type', 'file', 'type');
+        $this->createIndex('file-hash', 'file', 'hash');
+        $this->createIndex('file-filename', 'file', 'filename');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m190803_101632_alter_file cannot be reverted.\n";
+
+        return false;
+    }
+
+}
